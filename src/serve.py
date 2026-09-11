c3788b08a0b629b273a4150f46af1d4ce7a197ee
DianaVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2026-09-11 15:12:16 +0100

    broadcast new user joining

--- src/serve.py
+++ src/serve.py
@@ -3,13 +3,33 @@ from websockets.asyncio.server import serve
 from websockets.exceptions import ConnectionClosedOK
 import json
 
+CLIENTS = set()
+
+async def broadcastMessage(data):
+    message = ""
+    if("name" in data) :
+        message = data["name"] + ": "
+    else :
+        message = "Anonymous User: "
+    message = message + data["message"]
+    response = {"message": message}
+    for websocket in CLIENTS.copy():
+        try:
+            await websocket.send(json.dumps(response))
+        except ConnectionClosedOK:
+            pass
 
 async def handler(websocket):
+    CLIENTS.add(websocket)
     async for data in websocket:
         data = json.loads(data)
-        print(data)
-        if (("message" in data) & (data["message"] == "hello")):
-            await websocket.send(json.dumps({"message": "hello"}))
+        
+        if("message" in data) :
+            await broadcastMessage(data)
+            continue
+        
+        if("name" in data) :
+            await websocket.send(json.dumps({"message": "welcome to the chat, " + data["name"]}))
 
 async def main():
     print("Starting websocket server...")
